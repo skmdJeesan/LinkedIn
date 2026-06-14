a6f95b137ffb10e88b310ee730d8afe2c8ed4828
Sk Md Jeesan <skmdjeesan@gmail.com>
2026-06-14 17:10:32 +0530

    some renaming

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { register, login, logout } from "../controllers/userController.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 
 router.post("/register", register);
 router.post("/login", login);
